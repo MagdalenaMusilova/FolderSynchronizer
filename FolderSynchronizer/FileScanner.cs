7a914d72bee835e7669257dbe511368e0e054a45
musilmag <musilmag@fit.cvut.cz>
2025-07-05 12:35:09 +0200

    Naive implementation
    Currently the only synchronization is copying the full folder

---
+++ FolderSynchronizer/FileScanner.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Abstractions;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FolderSynchronizer
+{
+	internal class FileScanner
+	{
+		private readonly IFileSystem _fs;
+		private readonly int _chunkSize;
+
+
+		public FileScanner(IFileSystem fs, int chunkSize) {
+			_fs = fs;
+			_chunkSize = chunkSize;
+		}
+
+		public List<Chunk> SplitFileIntoChunks(string pathToFile) {
+			List<Chunk> chunks = new List<Chunk>();
+
+			var stream = _fs.File.OpenRead(pathToFile);
+			var buffer = new byte[_chunkSize];
+			while (true) {
+				int bytesRead = stream.Read(buffer, 0, _chunkSize);
+				if (bytesRead < 0) {
+					break;
+				}
+
+				string hash = GetHash(buffer);
+				chunks.Add(new Chunk() {
+					Hash = hash,
+					Size = bytesRead
+				});
+			}
+
+			return chunks;
+		}
+
+		public string GetFileChecksum(string pathToFile) {
+			var stream = _fs.File.OpenRead(pathToFile);
+			byte[] rawHash = MD5.HashData(stream);
+			return Convert.ToHexString(rawHash);
+		}
+
+		private string GetHash(byte[] bytes) {
+			byte[] rawHash = MD5.HashData(bytes);
+			return Convert.ToHexString(rawHash);
+		}
+	}
+}
---
+++ FolderSynchronizer/Manifest/ReplicaManifest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace FolderSynchronizer.Manifest
+{
+	public class ReplicaManifest
+	{
+		public required DateTime Created { get; set; }
+		public required DateTime Updated { get; set; }
+		public required string FolderPath { get; set; }
+		public required Dictionary<string, FileDetails> Files { get; set; }
+		public string? ManifestChecksum { get; set; }
+
+		[OnSerializing]
+		private void SetChecksum() {
+			ManifestChecksum = GetChecksum();
+		}
+
+		public string GetChecksum() {
+			string? tmp = ManifestChecksum;
+			ManifestChecksum = String.Empty;
+			string jsonString = JsonSerializer.Serialize(this);
+			ManifestChecksum = tmp;
+
+			byte[] rawHash = MD5.HashData(Encoding.UTF8.GetBytes(jsonString));
+			return Convert.ToHexString(rawHash);
+		}
+
+		public bool ChecksumMatches(string checksum) {
+			string thisChecksum = GetChecksum();
+			return thisChecksum == checksum;
+		}
+	}
+}
